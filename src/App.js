28a7ce692d5008996d516f73a3a9ca8abe3147e9
unknown <brosjayme@gmail.com>
2023-07-20 09:29:00 +0100

    commit in this repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("button clicked");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>do something</button>
+
+      <form onSubmit={handleClick}>
+        <input type="text" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
