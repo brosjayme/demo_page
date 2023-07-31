a6e236629bd11c28c2bd8e614a356ccf06c1b162
unknown <brosjayme@gmail.com>
2023-07-31 10:33:10 +0100

    commit in this repo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
-import Form from "./Form";
+import Jokes from "./Jokes";
+// import Form from "./Form";
 function App() {
-  function handleClick() {
-    console.log("button clicked");
-  }
-  function handleSubmit(e) {
-    console.log("Form submitted");
-  }
+  // function handleClick() {
+  //   console.log("button clicked");
+  // }
+  // function handleSubmit() {
+  //   console.log("Form submitted");
+
+  const dadJoke2 = "I used to be a banker but i lost interest.";
 
   return (
     <>
-      <div className="App">
-        <button onClick={handleClick}>do something</button>
-        <Form onSubmit={handleSubmit} />
-      </div>
+      <h1>Dad Joke</h1>
+      <Jokes />
+      <Jokes />
+      {/* <div className="App"> */}
+      {/* <button onClick={handleClick}>do something</button>
+        <Form onSubmit={handleSubmit} /> */}
+      {/* </div>  */}
     </>
   );
 }
---
+++ src/Jokes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const dadJoke = "I used to be a banker but i lost interest.";
+
+function Jokes() {
+  return (
+    <>
+      <p>{dadJoke}</p>
+      <p>***</p>
+    </>
+  );
+}
+
+export default Jokes;
